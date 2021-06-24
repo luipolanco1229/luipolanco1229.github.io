ebcbe371825708007753582f71283aa9bb8f1f0b
JoseTor <josetordecilla800@gmail.com>
2021-06-24 18:12:24 -0500

    Se terminaron los formularios, falta relacionarlos por links

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useState} from 'react';
+import Forms_Signup from './Form_Signup';
+// import FormsUp from "./Form_Signup";
 
 const Forms = () => {
 
@@ -12,43 +14,49 @@ const Forms = () => {
          console.log(event.target.value);
         setInformation ({
             ...Information,
-            [event.target.name] : event.target.value
+            [event.target.name] : event.target.value,
+            [event.target.password] : event.target.value
         })
     }
 
-    // const sendData = () => {
-    //     event.preventDefault()
+     const sendData = () => {
+    //    event.preventDefault()
     //     console.log('enviando datos...' + Information.user + ' ' + Information.password)
-    // }
+     }
 
 
      return(
          <div className="FormSignIn">
-             <h1 className="HeaderSigIn">Iniciar sesión </h1>
-             <form className="SigIn" onSubmit={sendData} >
+             <div className="HeaderSigIn">
+                 <h1>Iniciar sesión </h1>
+             </div>
+              <form className="SigIn" onSubmit={sendData} > 
+                
                  <div clasName="Items_form_signIn">
+                     <p>Correo o nombre usuario</p>
                      <input 
-                     placeholder="Ingrese su correo electrónico o nombre de usuario"
+                     placeholder="Ingrese su correo electrónico o usuario"
                      type="email"
                      name="user"
                      onChange={HandleInputChange}>
                      </input>
                  </div>
                  <div clasName="Items_form_signIn">
+                      <p>Contraseña</p>
                       <input 
                       placeholder="Ingrese su contraseña"
-                      type="text"
+                      type="password"
                       name="password"
                       onChange={HandleInputChange}>
 
                       </input>
                  </div>
-                 <div clasName="Items_form_signIn">
-                     <button className="Send_sigIn" type="submit">Iniciar sesión</button>
+                 <div clasName="div_send_sigIn">
+                     <button className="Send_signIn" type="submit">Iniciar sesión</button>
                  </div>
-                  Si no tienes una cuenta <i>Regístrate</i>
+                
+                 {/* <p>Si no tienes una cuenta <span class="messageForm"><a href={FormsUp}>Regístrate</a> </span></p> */}
              </form>
-            <h3>{Information.user} - {Information.password} </h3>
          </div>
      )
 }
