c9025e930019f6fa30083f50279d18ee8251fd62
luipolanco1229 <Luipolanco1229>
2021-08-12 19:53:46 -0500

    Cambios en el cdisÃeño de la página

--- src/Components/ProductCategoriesSection.js
+++ src/Components/ProductCategoriesSection.js
@@ -8,56 +8,59 @@ import { Link } from "react-router-dom";
 const ProductCategories = () => {
   return (
     <div className="background_section_product_categories">
-     
-      <Link className='link link--text-center' to='/tvs'>
-        <div className="div_category_product">
+      <div className="div_section_product_categories">
+
+      <div className="div_category_product">
+        <Link className="link link--text-center" to="/tvs">
           <div className="categoryProduct">
             <div className="div_img_category">
-            <CategoryTvs />
+              <CategoryTvs />
             </div>
           </div>
-          <div >
-            <h4> Televisores</h4>
+          <div>
+            <h4 className="categoryh4"> Televisores</h4>
           </div>
-        </div>
-      </Link>
-      <Link className='link link--text-center' to='/computers'>
-        <div className="div_category_product">
+        </Link>
+      </div>
+
+      <div className="div_category_product">
+        <Link className="link link--text-center" to="/computers">
           <div className="categoryProduct">
             <div className="div_img_category">
               <CategoryComputers />
             </div>
           </div>
-          <div >
-            <h4> Computadores</h4>
+          <div className="categoryh4">
+            <h4>Computadores</h4>
           </div>
-        </div>
-      </Link>
-      <Link className='link link--text-center' to='/smartphones'>
-        <div className="div_category_product">
+        </Link>
+      </div>
+      <div className="div_category_product">
+        <Link className="link link--text-center" to="/smartphones">
           <div className="categoryProduct">
             <div className="div_img_category">
               <CategorySmartphones />
             </div>
           </div>
-          <div >
+          <div>
             <h4> Smartphones</h4>
           </div>
+         </Link>
         </div>
-      </Link>
-      <Link className='link link--text-center' to='/gadgets'>
+     
         <div className="div_category_product">
+         <Link className='link link--text-center' to='/gadgets'>
           <div className="categoryProduct">
             <div className="div_img_category">
-            <CategoryGadgets />
+              <CategoryGadgets />
             </div>
           </div>
-          <div >
+          <div>
             <h4> Gadgets</h4>
           </div>
-        </div>
-      </Link>
-      
+        </Link>
+      </div>
+      </div>
     </div>
   );
 };
